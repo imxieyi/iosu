992661427ed1c46825058a1b60b293e2ad99fed8
imxieyi <wez733@live.cn>
2017-05-12 01:18:28 +0800

    Render hit circle & judge

---
+++ iosu/Scoring.swift
@@ -0,0 +1,16 @@
+//
+//  Scoring.swift
+//  iosu
+//
+//  Created by xieyi on 2017/5/11.
+//  Copyright © 2017年 xieyi. All rights reserved.
+//
+
+import Foundation
+
+enum HitResult {
+    case S300
+    case S100
+    case S50
+    case Fail
+}
